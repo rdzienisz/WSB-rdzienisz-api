e4b250eda96d2603c7fd0da9eab32eca607626e7
robeat1 <dzienisz.robert@gmail.com>
2023-12-04 19:22:03 +0100

    Added entities

---
+++ src/main/java/com/rdzienisz/university/entities/Company.java
@@ -0,0 +1,31 @@
+package com.rdzienisz.university.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private double budget;
+
+    @OneToMany(mappedBy = "company")
+    private List<Employee> employees;
+}
---
+++ src/main/java/com/rdzienisz/university/entities/Customer.java
@@ -0,0 +1,31 @@
+package com.rdzienisz.university.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private double money;
+
+    @ManyToMany
+    private List<Company> companies;
+}
